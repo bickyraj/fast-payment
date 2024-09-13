e29b9a80c483072df7f6edd30ebb39417cb1d454
bicky raj kayastha <bickyrajkayastha@gmail.com>
2024-09-13 22:11:05 +0200

    updates

--- frontend-service/src/main/java/com/bicky/demopayment/frontendservice/frontend/entrypoint/view/PaymentController.java
+++ frontend-service/src/main/java/com/bicky/demopayment/frontendservice/frontend/entrypoint/view/PaymentController.java
@@ -1,6 +1,5 @@
 package com.bicky.demopayment.frontendservice.frontend.entrypoint.view;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
