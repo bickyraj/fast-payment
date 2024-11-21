13cab253f8b358914af19c35b5b24f5f0ce6a9cc
bicky raj <bickyrajkayastha@gmail.com>
2024-11-21 21:43:14 +0100

    updates

--- service-registry/src/main/java/com/bicky/demopayment/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/bicky/demopayment/serviceregistry/ServiceRegistryApplication.java
@@ -10,6 +10,6 @@ public class ServiceRegistryApplication {
     public static void main(String[] args)
     {
         SpringApplication.run(ServiceRegistryApplication.class, args);
-        System.out.println("Hello from eureka server!");
+        System.out.println("Hello from eureka server!!");
     }
 }
