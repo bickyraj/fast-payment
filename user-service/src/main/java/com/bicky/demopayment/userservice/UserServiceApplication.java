a156f0418471514b77e21c16ef5d6342124a1ea2
bicky raj <bickyrajkayastha@gmail.com>
2024-11-21 22:49:59 +0100

    updates

--- user-service/src/main/java/com/bicky/demopayment/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/bicky/demopayment/userservice/UserServiceApplication.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+        System.out.println("Hello World!");
     }
 }
