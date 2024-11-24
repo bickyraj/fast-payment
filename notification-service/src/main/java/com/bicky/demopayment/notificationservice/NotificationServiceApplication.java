3cf89a280269277fd7af9967a6d790c05b663b44
bicky <bickyrajkayastha@gmail.com>
2024-11-24 22:29:23 +0100

    updates

--- notification-service/src/main/java/com/bicky/demopayment/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/bicky/demopayment/notificationservice/NotificationServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
-        System.out.println("hello new world from notification service!!");
+        System.out.println("hello new world");
     }
 
 //    @EventListener(ApplicationReadyEvent.class)
--- notification-service/src/main/java/com/bicky/demopayment/notificationservice/dto/NotificationRequestBody.java
+++
@@ -1,9 +0,0 @@
-package com.bicky.demopayment.notificationservice.dto;
-
-import lombok.Data;
-
-@Data
-public class NotificationRequestBody {
-    private String title;
-    private String body;
-}
