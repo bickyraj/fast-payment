dd7e403f40ceccab41293779c2d89845a4d9306d
bicky <bickyrajkayastha@gmail.com>
2024-11-28 19:46:40 +0100

    updats

--- api-gateway/src/main/java/com/bicky/demopayment/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bicky/demopayment/apigateway/ApiGatewayApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-        System.out.println("Hello World!");
+        System.out.println("Hello World from api gateway!");
     }
 }
