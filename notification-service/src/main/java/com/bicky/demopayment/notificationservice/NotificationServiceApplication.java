06ed99986b55bb7656245e510254100cb3334a20
bicky <bickyrajkayastha@gmail.com>
2024-11-29 23:09:39 +0100

    updates

--- notification-service/src/main/java/com/bicky/demopayment/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/bicky/demopayment/notificationservice/NotificationServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 //    @EventListener(ApplicationReadyEvent.class)
